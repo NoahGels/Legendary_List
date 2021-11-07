7a1711fab3f98a5df0f0c34ca111c97c295e5dde
NoahGels <noah.gels@gg-lin.de>
2021-11-07 11:19:19 +0100

    Finished Project

---
+++ config/config.js
@@ -0,0 +1,10 @@
+
+
+export const databaseConfig = {
+  host: 'j0bit.com',
+  user:'drinkbill_admin',
+  password: '4CuW5qeeA6FHTe8QLXWygTBpMpZF2UnE',
+  connectionLimit: 5,
+  port: 3306,
+  database: 'drinkbill',
+}
